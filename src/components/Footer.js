8b661c092c981a0cdb8c31d61bd7a919f79c951e
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2024-10-28 22:57:40 +0530

    new2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
-import { FaPhoneAlt, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -18,15 +17,12 @@ const Footer = () => {
         </div>
         
         <div className="nav-right-down">
-          <div className="call-info">
-            <FaPhoneAlt className="call-icon" />
-            <span className="contact-text">+91-9876543210(MOHAN)</span>
-          </div>
-          
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="nav-icon">
-            <FaInstagram className="social-icon" />
-            <FaWhatsapp className="social-icon" />
-          </a>
+        <div className="nav-left-down">
+          <Link to="/contact" className="nav-link">Contact Us</Link>
+          <Link to="/about" className="nav-link">About Us</Link>
+          <Link to="/faq" className="nav-link">FAQ</Link>
+        </div>
+         
         </div>
       </footer>
     </>
