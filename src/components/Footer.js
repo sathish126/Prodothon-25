7304da8dfc9dfa120a4f173dcee697b75139b9d6
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2024-10-28 23:14:03 +0530

    new3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 // src/components/Footer.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import './Footer.css'; 
 
 const Footer = () => {
   return (
@@ -10,19 +10,10 @@ const Footer = () => {
       <div className="footer-line"></div>
       
       <footer className="footer">
-        <div className="nav-left-down">
+        <div className="nav-links">
           <Link to="/contact" className="nav-link">Contact Us</Link>
           <Link to="/about" className="nav-link">About Us</Link>
           <Link to="/faq" className="nav-link">FAQ</Link>
-        </div>
-        
-        <div className="nav-right-down">
-        <div className="nav-left-down">
-          <Link to="/contact" className="nav-link">Contact Us</Link>
-          <Link to="/about" className="nav-link">About Us</Link>
-          <Link to="/faq" className="nav-link">FAQ</Link>
-        </div>
-         
         </div>
       </footer>
     </>
